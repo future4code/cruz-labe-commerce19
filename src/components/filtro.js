94b69487d535bf5cd471e145b183fea320c5a270
unknown <jcostadelli@gmail.com>
2021-03-18 01:34:48 -0300

    'caixa' de filtros

--- src/components/filtro.js
+++ src/components/filtro.js
@@ -1,6 +1,27 @@
 import React from "react";
+import "./filtro.css"; 
 
-<div>
-    <h2>Filtros</h2>
+export default function App() {
+  return (
+    <div className="App">
+      <div className="Caixa">
+      <h3>Filtros</h3>
+  <label class = "valor-minimo"> 
+      <p>Valor Mínimo:</p>
+      <input type={Number}></input>
+  </label>
 
-</div>
+  <label class = "valor-maximo"> 
+      <p>Valor Máximo:</p>
+      <input type={Number}></input>
+  </label>
+
+  <label class = "valor-minimo"> 
+      <p>Busca por nome:</p>
+      <input type={Text}></input>
+  </label>
+
+    </div>
+    </div>
+  );
+}
